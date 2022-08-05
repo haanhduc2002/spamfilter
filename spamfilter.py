7093c40edb45082c5f78e57953b8df7d6e80c659
Hà Anh Đức <anhducha.work@gmail.com>
2022-08-05 17:07:21 +0700

    Add files via upload

--- spamfilter.py
+++ spamfilter.py
@@ -17,17 +17,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 app = FastAPI()
 
 
+# Preparing text data
 def handle_message(message): 
-  # Step 1: Lemmatization
+  # Step 1: Remove puntuation 
+  message1 = [char for char in message if char not in string.punctuation]
+  message1 = ''.join(message1)
+  # Step 2: Remove stop word
+  message2 = [word for word in message1.split() if word.lower() not in stopwords.words('english')]
+  message2 = ' '.join(message2)
+  # Step 3: Lemmatization
   lemmatizer = WordNetLemmatizer()
-  word_list = nltk.word_tokenize(message)
-  message1 = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
-  # Step 2: Remove puntuation 
-  message2 = [char for char in message1 if char not in string.punctuation]
-  message2 = ''.join(message2)
-  # Step 3: Remove stop word
-  message3 = [word for word in message2.split() if word.lower() not in stopwords.words('english')]
-  result = ' '.join(message3)
+  word_list = nltk.word_tokenize(message2)
+  result = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
   return result
 
 
